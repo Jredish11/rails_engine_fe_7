cb94f8ec5f58b6a624c3b5634de1c46edd1cc421
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-20 14:10:30 -0600

    add tests that shows merchants as links and redirects to merchants show page

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -11,8 +11,33 @@ RSpec.describe "Merchant index" do
   describe "when i visit /merchants" do
     it "lists all of the merchants by name" do
       visit "/merchants"
+
+      expect(page).to have_content("62")
+      expect(page).to have_content("merchant")
+      expect(page).to have_content("Schroeder-Jerde")
+      expect(page).to_not have_content("101")
+      expect(page).to_not have_content("customer")
+      expect(page).to_not have_content("Yankee Doodle")
+    end
+
+    it "shows all merchant names as links that redirect to /merchants/:id, individual show page" do
+      attrs = {
+        id: "1",
+        type: "merchant",
+        attributes: {
+          name: "Schroeder-Jerde"
+        }
+      }
+  
+      merchant = Merchant.new(attrs)
+
+      visit "/merchants"
+
       
-      expect(page).to have_content("")
+      expect(page).to have_link(merchant.name)
+      click_link(merchant.name)
+      
+      expect(current_path).to eq(merchant_path(merchant.id))
     end
   end
 end
