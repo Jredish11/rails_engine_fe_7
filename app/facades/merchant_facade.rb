7a14cb4d2a99c59b9059bc8b38969d8d2ab1f217
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-20 17:27:22 -0600

    add find_merchant method

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -4,4 +4,11 @@ class MerchantFacade
       Merchant.new(merchant)
     end
   end
+
+  def self.find_merchant(merchant_id)
+    merchant_data = MerchantService.merchant_items.find do |merchant|
+      merchant[:id] == merchant_id
+      Merchant.new(merchant_data) if merchant_data
+    end
+  end
 end
