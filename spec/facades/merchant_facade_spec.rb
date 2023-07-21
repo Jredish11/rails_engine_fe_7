33896ba3506e54a739827fe570eb539ee382a172
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-20 18:15:13 -0600

    add tests for merchant_items facacde method

--- spec/facades/merchant_facade_spec.rb
+++ spec/facades/merchant_facade_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe MerchantFacade do
-  describe 'class methods' do
-    describe '#merchants_list' do
-      it 'returns the merchants data' do
+  describe "class methods" do
+    describe "#merchants_list" do
+      it "returns the merchants data" do
         merchants = MerchantFacade.merchants_list
 
         expect(merchants).to be_an(Array)
@@ -13,6 +13,27 @@ RSpec.describe MerchantFacade do
         expect(merchants.first.id).to be_a(String)
         expect(merchants.first.type).to be_a(String)
         expect(merchants.first.name).to be_a(String)
+      end
+    end
+
+    describe "#merchant_items(id)" do
+      it "returns all the items the merchant sells" do
+        attrs = {
+          id: "1",
+          type: "merchant",
+          attributes: {
+            name: "Schroeder-Jerde"
+          }
+        }
+        
+        merchant = Merchant.new(attrs)
+        id = merchant.id
+        items = MerchantFacade.merchant_items(id)
+
+        expect(items.first.id).to be_a(String)
+        expect(items.first.name).to be_a(String)
+        expect(items.first.type).to be_a(String)
+        expect(items.first.name).to eq("Item Nemo Facere")
       end
     end
   end
